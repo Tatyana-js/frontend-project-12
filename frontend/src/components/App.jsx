b862d90d69bc4d97c57007306f2cd48bda4db6a7
Татьяна Андреева <tan-andreeva@mail.ru>
2024-12-11 22:23:46 +0300

    add .

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,27 +1,3 @@
-import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './Home';
-import About from './About';
-
-const App = () => {
-  return (
-    <Router>
-      <div>
-        <Switch>
-          <Route exact path="/src/components/Home.jsx">
-            <Home />
-          </Route>
-          <Route path="/src/components/About.jsx">
-            <About />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
-  );
-};
+const App = () => <h1>Hexlet Chat</h1>;
 
 export default App;
-
-// const App = () => <h1>Hexlet Chat</h1>;
-
-// export default App;
