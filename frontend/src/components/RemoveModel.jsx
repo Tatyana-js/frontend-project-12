158b67e7e4b7efa28490efe25f07bff50ef58105
Татьяна Андреева <tan-andreeva@mail.ru>
2025-02-10 21:40:20 +0400

    add removeModal

--- frontend/src/components/RemoveModel.jsx
+++ frontend/src/components/RemoveModel.jsx
@@ -1,20 +1,17 @@
 import { Modal, Button } from 'react-bootstrap';
 import { useTranslation } from 'react-i18next';
 import { useRemoveChannelMutation } from '../api/chatApi';
-import { useSelector, useDispatch } from 'react-redux';
+import { useSelector } from 'react-redux';
 import { toast } from 'react-toastify';
-import { selectActiveTab, defaultChannel } from '../slices/activeChannelSlice.js';
 
 const RemoveChannel = ({ onHide }) => {
   const { t } = useTranslation();
   const [ removeChannel ] = useRemoveChannelMutation();
-  const dispatch = useDispatch();
   const channel = useSelector((state) => state.modals.channel);  
 
   const handleRemove = async (id) => {
     try {
       await removeChannel(id);
-      dispatch(selectActiveTab(defaultChannel));
       toast.success(t('channels.delete'));
       onHide();
     } catch (err) {
@@ -32,10 +29,10 @@ return (
       </Modal.Header>
         <Modal.Body>
           <p className='lead'>{t('modal.removeText')}</p>
-              <div className='d-flex justify-content-end'>
-                <Button type="button" className='me-2' variant="secondary" onClick={onHide}>{t('modal.cancel')}</Button> 
-                <Button type="submit" variant="danger" onClick={() => handleRemove(channel.id)}>{t('modal.removeButton')}</Button> 
-              </div>
+            <div className='d-flex justify-content-end'>
+              <Button type="button" className='me-2' variant="secondary" onClick={onHide}>{t('modal.cancel')}</Button> 
+              <Button type="submit" variant="danger" onClick={() => handleRemove(channel.id)}>{t('modal.removeButton')}</Button> 
+            </div>
         </Modal.Body>
     </Modal.Dialog>
   </div>
